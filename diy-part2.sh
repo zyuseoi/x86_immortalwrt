2fb9ee0e771316c6fbfb7bb7e5bea16a90e9f14e
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-09-23 10:00:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,27 +11,4 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-
-
-mkdir -p package/base-files/files/etc/config
-
-# 写入 network 配置
-cat > package/base-files/files/etc/config/network <<EOF
-config device
-    option name 'br-lan'
-    option type 'bridge'
-    list ports 'eth0'
-    list ports 'eth2'
-
-config interface 'lan'
-    option device 'br-lan'
-    option proto 'static'
-    option ipaddr '192.168.50.1'
-    option netmask '255.255.255.0'
-
-config interface 'wan'
-    option device 'eth1'
-    option proto 'dhcp'
-EOF
-
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
